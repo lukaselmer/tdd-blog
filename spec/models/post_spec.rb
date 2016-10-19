777c80022f7635303a08ccd54357ba0b69024e89
Lukas Elmer <lukas.elmer@renuo.ch>
2016-10-19 22:41:11 +0200

    refactoring

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  def expect_post(attributes = {})
+    expect(build_stubbed(:post, attributes))
+  end
+
   describe 'validations' do
-    it { expect(build_stubbed(:post)).to be_valid }
-    it { expect(build_stubbed(:post, title: '')).not_to be_valid }
-    it { expect(build_stubbed(:post, content: '')).not_to be_valid }
+    it { expect_post.to be_valid }
+    it { expect_post(title: '').not_to be_valid }
+    it { expect_post(content: '').not_to be_valid }
   end
 end
